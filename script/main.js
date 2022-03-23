18c5fe4e5e47768a1878aeb2eace60037ecd8a47
ismailow <ismailovskiy2@gmail.com>
2022-03-23 13:32:57 +0300

    delete function rework

--- script/main.js
+++ script/main.js
@@ -22,28 +22,27 @@ const addTask = function(){
 
     modalAdd.addEventListener('click', () => {
         let task = document.createElement('div');
+        task.classList.add('task')
         task.innerHTML = `
-            <div class="task">
-                <div class="task__text">
-                    <div class="task__description">${modalInput.value}</div>
+            <div class="task__text">
+                <div class="task__description">${modalInput.value}</div>
 
-                    <div class="task__control">
-                        <input type="checkbox">
-                    </div>
+                <div class="task__control">
+                    <input type="checkbox">
                 </div>
+            </div>
 
-                <div class="task__controls">
-                    <button class="task__btn" class="impotant">
-                        <svg class="task__icon">
-                            <use xlink:href="#important"></use>
-                        </svg>
-                    </button>
-                    <button class="task__btn delete">
-                        <svg class="task__icon">
-                            <use xlink:href="#delete"></use>
-                        </svg>
-                    </button>
-            </div>`;
+            <div class="task__controls">
+                <button class="task__btn" class="impotant">
+                    <svg class="task__icon">
+                        <use xlink:href="#important"></use>
+                    </svg>
+                </button>
+                <button class="task__btn delete-task">
+                    <svg class="task__icon">
+                        <use xlink:href="#delete"></use>
+                    </svg>
+                </button>`;
         
         tasks.appendChild(task);
         modal.classList.add('_hide');
@@ -76,7 +75,7 @@ const toggleTabs = function() {
 
 const isEmpty = function() {
     screens.forEach((item) => {
-        let tasksList = item.children;
+        let tasksList = item.querySelectorAll('.task');
         let empty = document.createElement('div');
         empty.innerHTML = `
             <div class="empty">
@@ -94,21 +93,40 @@ const isEmpty = function() {
         item.addEventListener('DOMNodeInserted', () => {
             item.removeChild(empty);
         });
+
+        // item.addEventListener('DOMNodeRemoved', (event) => {
+        //     if(tasksList.length === 0){
+        //         console.log(tasksList.length);
+        //     };
+        // });
     });
 };
 
 const deleteTask = function(){
     const tasks = document.querySelector('.tasks');
+    const modalDelete = document.querySelector('._delete');
+    const deleteBtn = document.querySelector('.delete-modal');
+    const cancelBtn = document.querySelector('.cancel')
+
     let tasksList;
     
     tasks.addEventListener('DOMNodeInserted', () => {
         tasksList = tasks.querySelectorAll('.task');
 
         tasksList.forEach((item) => {
-            const deleteBtn = item.querySelector('.delete');
+            const openModal = item.querySelector('.delete-task');
 
-            deleteBtn.addEventListener('click', () => {
-                item.remove();
+            openModal.addEventListener('click', () => {
+                modalDelete.classList.remove('_hide');
+
+                deleteBtn.addEventListener('click', () => {
+                    item.remove();
+                    modalDelete.classList.add('_hide');
+                });
+
+                cancelBtn.addEventListener('click', () => {
+                    modalDelete.classList.add('_hide');
+                });
             });
         });
     }); 
